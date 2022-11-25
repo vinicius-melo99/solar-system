0307aed5af926425595e4e96b7f2198ad83a5897
Vinícius Melo <viniciusmelo317@gmail.com>
2022-11-25 15:05:05 -0300

    requisitos 3 e 4 ok

---
+++ src/components/Title.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+
+class Title extends Component {
+  render() {
+    const { headline } = this.props;
+
+    return (
+      <h2>{headline}</h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: Proptypes.string,
+};
+
+Title.defaultProps = {
+  headline: '',
+};
+
+export default Title;
